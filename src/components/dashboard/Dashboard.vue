47fe82731a21e961f772a6a855160d268138885d
VerseVent <mike.82pronka.27a@gmail.com>
2022-11-15 11:21:33 +0100

    #12 fix icons sizes, add more icons

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,40 +1,56 @@
 <template>
   <div class="container overflow-hidden py-4">
-    <div
-      class="row gy-4 gx-4 justify-content-evenly text-center align-items-center"
-    >
-      <div class="col-md-4">
+    <div class="row gy-4 gx-4 text-center align-items-center">
+      <div class="col-md-4 col-lg-3">
         <router-link class="text-decoration-none" to="/quiz">
-          <div class="dashboard__activity rounded-4 p-3">
-            <span class="icon-form w-75"/>
+          <div class="dashboard__activity border rounded-4 p-3">
+            <span class="icon-form w-75" />
             <h1 class="text-primary fs-3 mt-4">Create quiz</h1>
           </div>
         </router-link>
       </div>
-      <div class="col-md-4 offset-md-1">
+      <div class="col-md-4 col-lg-3">
+        <router-link class="text-decoration-none" to="/categories">
+          <div class="dashboard__activity border rounded-4 p-3">
+            <span class="icon-book-open" />
+
+            <h1 class="text-primary fs-3 mt-4">Questions</h1>
+          </div>
+        </router-link>
+      </div>
+      <div class="col-md-4 col-lg-3">
+        <router-link class="text-decoration-none" to="/results">
+          <div class="dashboard__activity border rounded-4 p-3">
+            <span class="icon-paste" />
+
+            <h1 class="text-primary fs-3 mt-4">Results</h1>
+          </div>
+        </router-link>
+      </div>
+      <div class="col-md-4 col-lg-3">
+        <router-link class="text-decoration-none" to="/candidates">
+          <div class="dashboard__activity border rounded-4 p-3">
+            <span class="icon-users" />
+
+            <h1 class="text-primary fs-3 mt-4 text-unstyled">Candidates</h1>
+          </div>
+        </router-link>
+      </div>
+      <div class="col-md-4 col-lg-3">
+        <router-link class="text-decoration-none" to="/">
+          <div class="dashboard__activity border rounded-4 p-3">
+            <span class="icon-loop-alt" />
+
+            <h1 class="text-primary fs-3 mt-4 text-unstyled">Last result</h1>
+          </div>
+        </router-link>
+      </div>
+      <div class="col-md-4 col-lg-3">
         <router-link class="text-decoration-none" to="/statistics">
-          <div class="dashboard__activity rounded-4 p-3">
-            <span class="icon-stats"/>
+          <div class="dashboard__activity border rounded-4 p-3">
+            <span class="icon-stats" />
 
             <h1 class="text-primary fs-3 mt-4">Statistics</h1>
-          </div>
-        </router-link>
-      </div>
-      <div class="col-md-4">
-        <router-link class="text-decoration-none" to="/dashboard">
-          <div class="dashboard__activity rounded-4 p-3">
-            <span class="icon-nophoto"/>
-
-            <h1 class="text-primary fs-3 mt-4">Undefined</h1>
-          </div>
-        </router-link>
-      </div>
-      <div class="col-md-4 offset-md-1">
-        <router-link class="text-decoration-none" to="/dashboard">
-          <div class="dashboard__activity rounded-4 p-3">
-            <span class="icon-nophoto"/>
-
-            <h1 class="text-primary fs-3 mt-4 text-unstyled">Undefined</h1>
           </div>
         </router-link>
       </div>
