809ac7a6f3c89c21a404baba746b4ffe191f6a10
ira-shumakova <iryna.shumakova@sigma.software>
2022-11-18 00:30:40 +0200

    fix collapse #2

--- src/components/results/CategoryListItem.vue
+++ src/components/results/CategoryListItem.vue
@@ -14,16 +14,20 @@ const props = defineProps({
         class="btn btn-outline-primary mt-2"
         type="button"
         data-bs-toggle="collapse"
-        :data-bs-target="'#multiCollapseExample1' + props.item.trim()"
+        :data-bs-target="
+          '#multiCollapseExample1' + props.item.split(' ').join('')
+        "
         aria-expanded="false"
-        :aria-controls="'multiCollapseExample1' + props.item.trim()"
+        :aria-controls="
+          'multiCollapseExample1' + props.item.split(' ').join('')
+        "
       >
         {{ props.item }}
       </button>
     </p>
     <div
       class="collapse multi-collapse"
-      :id="'multiCollapseExample1' + props.item.trim()"
+      :id="'multiCollapseExample1' + props.item.split(' ').join('')"
     >
       <div class="card card-body bg-light">
         <ul class="list-unstyled">
