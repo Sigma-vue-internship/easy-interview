d4804f9eacdcef864868a51d097039306b4cf414
VerseVent <mike.82pronka.27a@gmail.com>
2022-11-24 10:18:34 +0100

    #56 fix sonar

--- src/components/results/ListItem.vue
+++ src/components/results/ListItem.vue
@@ -10,10 +10,10 @@ defineProps({
 </script>
 
 <template>
-  <!-- //NOSONAR -->
   <li
     class="border border-secondary mt-3 p-2 rounded-3 text-secondary mx-auto shadow-sm row"
   >
+    <!-- //NOSONAR -->
     <div class="col-12 col-md-9 ps-3">
       <div class="row">
         <p class="text-center text-md-start m-0">{{ oneQuestion.question }}</p>
